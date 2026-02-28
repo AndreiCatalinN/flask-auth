603994d4f7b0594d21469274a688dc069151a746
AndreiCatalinN <negura.a.catalin@gmail.com>
2026-02-28 20:34:25 +0200

    fixed login bug

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,9 +3,15 @@ from flask import Flask, render_template
 
 from . import db
 from . import auth
+from . import blog
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(
+        __name__, 
+        instance_relative_config=True,
+        template_folder="./templates",
+        static_folder="./static"
+    )
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
@@ -28,6 +34,9 @@ def create_app(test_config=None):
     
     db.init_app(app)
     app.register_blueprint(auth.bp)
+
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
 
